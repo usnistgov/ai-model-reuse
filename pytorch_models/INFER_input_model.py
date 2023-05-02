2d3691856896e43df834f45b2dad9c2d6218b5f9
PushkarSathe <pushkar.sathe@nist.gov>
2023-05-01 21:16:05 -0400

    AI models
    INFER_input_model.py:
    1. Added torchsummaryX for when torchsummary doesn't work.
    2. INFERFeatureExtractor:
       a. outputchannels set to 3.
       b. removed batchnormalization.
       c. added Conv2D layer and activation.
    3. INFERFeatureExtractor1D:
       a. Implemented. Takes in input and output channels. Similar architecture as INFERFeatureExtractor but uses Conv1D. Intermediate layer has 300 channels.
       b. In forward pass: data is reshaped before going through layers and reshaped back after layers.
    4. INFERDefaultModel:
       a. A minimal model with a single conv2d layer that can be attached to FeatureExtractor when no standard model is provided as input.
    5. CombinedModel:
       a. Added a window_size parameter as input for defaultmodel
       b. Updated class parameters.
       c. Updated forward pass to handle both options.
    6. LSTMModel:
       a. Implemented. Has LSTM layer with variable hidden size (2 default). and a Conv2D layer.
       b. forward pass involves permuting and reshaping data, passing through each layer and permuting and reshaping it back.
    7. FCNModel and SquishModel:
       a. Partially implemented.

--- pytorch_models/INFER_input_model.py
+++ pytorch_models/INFER_input_model.py
@@ -1,5 +1,6 @@
 import torch
 
+from torchsummaryX import summary as summaryX  #: For asserting model input output layer dimensions
 from torchsummary import summary  #: For asserting model input output layer dimensions
 
 
@@ -17,23 +18,89 @@ class INFERFeatureExtractor(torch.nn.Module):
     def __init__(self, input_channels):
         super(INFERFeatureExtractor, self).__init__()
         self.input_channels = input_channels
-        # self.height = shape[-2]
-        # self.width = shape[-1]
+        self.output_channels = 3
 
         self.layer1 = torch.nn.Conv2d(self.input_channels, 32, kernel_size=3, stride=1, padding=1)
-        self.bn1 = torch.nn.BatchNorm2d(32)
+        # self.bn1 = torch.nn.BatchNorm2d(32)
         self.activation1 = torch.nn.ReLU(inplace=True)
         # self.pool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
         self.layer2 = torch.nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
+        self.activation2 = torch.nn.ReLU(inplace=True)
         # self.layer2 = torch.nn.Conv2d(in_channels=6, out_channels=1, kernel_size=(6, 5), stride=1,
         #                               padding='same')
 
     def forward(self, x):
         x = self.layer1(x)
-        x = self.bn1(x)
+        # x = self.bn1(x)
         x = self.activation1(x)
         # x = self.pool1(x)
         x = self.layer2(x)
+        x = self.activation2(x)
+        return x
+
+
+class INFERFeatureExtractor1D(torch.nn.Module):
+    """
+    This model is made for the purpose of using 3 dimensional data as inputs
+    where the last 2 dimensions are X and Y axes of images.
+    TODO: add support for upto 5 dimensions.
+
+    concatenating with other publicly available models.
+    This model is not intented to be used on its own
+
+    """
+
+    def __init__(self, input_channels, standalone=True, output_channels=3):
+        super(INFERFeatureExtractor1D, self).__init__()
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+
+        self.layer1 = torch.nn.Conv1d(self.input_channels, 300, kernel_size=3, stride=1, padding='same')
+        # self.bn1 = torch.nn.BatchNorm1d(300)  # TODO: norm along an axis or 2D only?
+
+        self.activation1 = torch.nn.ReLU(inplace=True)
+        # self.pool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        self.layer2 = torch.nn.Conv1d(300, self.output_channels, kernel_size=1, stride=1, padding='same')
+        self.activation2 = torch.nn.ReLU(inplace=True)
+        # self.layer2 = torch.nn.Conv2d(in_channels=6, out_channels=1, kernel_size=(6, 5), stride=1,
+        #                               padding='same')
+
+    def forward(self, x):
+        batch, xis, xs, ys = x.shape
+        x = x.reshape((batch, xis, -1))
+        x = self.layer1(x)
+        # x = self.bn1(x)
+        x = self.activation1(x)
+        # x = self.pool1(x)
+        x = self.layer2(x)
+        x = self.activation2(x)
+        # print(x.shape)
+        x = x.reshape((batch, self.output_channels, xs, ys))
+        return x
+
+
+class INFERDefaultModel(torch.nn.Module):
+    """
+    A simple 2 layer fcn to be used as a default fallback model when no standard model is provided.
+    """
+
+    def __init__(self, window_size, output_channels, batch=80):
+        super(INFERDefaultModel, self).__init__()
+        self.window_size = window_size
+        self.output_channels = output_channels
+        # self.layer1 = torch.nn.Linear(in_features=3 * self.window_size * self.window_size * batch, out_features=400) # limited for size
+        # self.activation1 = torch.nn.ReLU(inplace=True)
+        # self.dropout1 = torch.nn.Dropout(0.25)
+        # self.layer2 = torch.nn.Linear(in_features=400, out_features=self.output_channels)
+        self.layer1 = torch.nn.Conv2d(3, out_channels=self.output_channels, kernel_size=3, stride=1,
+                                      padding=1)  # limited for size
+        self.activation1 = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        # print(f"input: {x.shape}")
+        x = self.layer1(x)
+        x = self.activation1(x)
+        # print(f"output: {x.shape}")
         return x
 
 
@@ -48,29 +115,105 @@ class CombinedModel(torch.nn.Module):
 
     """
 
-    def __init__(self, input_shape, segmentation_model, n_classes):
+    def __init__(self, input_shape, segmentation_model, n_classes, window_size=200, batchsize=80):
+        """
+        window size is only relevant for when there is no
+        """
         super(CombinedModel, self).__init__()
         self.models = []
+        self.window_size = window_size
         self.feature_extractor = INFERFeatureExtractor(input_shape)
-        # self.segmentation_model = models.segmentation.deeplabv3_resnet50(num_classes=n_classes, pretrained=True)
-        self.segmentation_model = segmentation_model
-        # self.concat_layer = torch.nn.Conv2d(128, 256, kernel_size=1, stride=1, padding=0)
-        # self.final_layer = torch.nn.Conv2d(256, n_classes, kernel_size=1, stride=1, padding=0)
+        self.input_channels = self.feature_extractor.input_channels
+        self.outputchannels = n_classes
 
-    # def combine(self, *listofmodels):
-    #     self.models.append(listofmodels)  # TEST
+        if segmentation_model is not None:
+            self.segmentation_model = segmentation_model
+        else:
+            self.segmentation_model = INFERDefaultModel(self.window_size, self.outputchannels, batch=batchsize)
+
+        # print(f"Generated Combined model: {CombinedModel} "
+        #       f"using FeatureExtractor: {self.feature_extractor} and Segmentation Model: {self.segmentation_model}")
 
     def forward(self, x):
         features = self.feature_extractor(x)
+        # segmentation_output = None
         segmentation_output = self.segmentation_model(features)
-        segmentation_output = segmentation_output['out']
+        try:
+            segmentation_output = segmentation_output['out']
+        except:
+            segmentation_output = segmentation_output.squeeze()
+            # segmentation_output = segmentation_output.type(torch.cuda.LongTensor)  # TODO
+            # print("segmentation output:", segmentation_output.shape, type(segmentation_output))
         # concat_output = torch.cat((features, segmentation_output), dim=1)
         # concat_output = self.concat_layer(concat_output)
         # final_output = self.final_layer(concat_output)
         return segmentation_output
-        # for selected_model in self.models:
-        #     x = selected_model(x)
         # return x
+
+
+class LSTMModel(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+
+        self.lstm = torch.nn.LSTM(self.input_size, self.hidden_size, self.num_layers, batch_first=True)
+        # self.drop = torch.nn.Dropout()
+        self.conv = torch.nn.Conv2d(self.hidden_size, self.num_classes, kernel_size=1, stride=1, padding='same')
+
+    def forward(self, x):
+        # x = (batch, xi, x, y) --> (xi, # TODO: is dim permutation required?
+        # x = x.permute(0, 2, 1, 3)
+        batchsize, xis, xs, ys = x.size()  # torch size not np
+        x = x.permute(0, 2, 3, 1)
+        x = x.reshape((batchsize, -1, xis))
+        ###################################################################################
+        # x = x.permute(1, 0, 2, 3)  # bring all dimensions except xi together
+        # x = x.reshape((xis, -1))  # combine those dimensions
+        # x = x.permute((1, 0))  # bring batch back to the front
+        # x = x.reshape(x.shape+(self.input_size,))
+        ###################################################################################
+        # print("XSIZE: ", x.size())
+        # h0 = torch.zeros(self.num_layers, batchsize, self.hidden_size).to(x.device)
+        # c0 = torch.zeros(self.num_layers, batchsize, self.hidden_size).to(x.device)
+        # input (x) should be of dims : (batchsize, seq. length, input_size)
+        h0 = torch.zeros(self.num_layers, batchsize, self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, batchsize, self.hidden_size).to(x.device)
+        x, (hn, cn) = self.lstm(x, (h0, c0))  # xshape = (newbatchsize, xis, hiddensize)
+        # print(x.shape, hn.shape, cn.shape)
+        x = x.permute(0, 2, 1)
+        x = x.reshape(batchsize, self.hidden_size, xs, ys)
+        # print(x.shape)
+        # x = x.reshape(batchsize, xs, ys, self.hidden_size, xis, 1)
+        x = self.conv(x)
+        return x
+
+
+class FCNModel(torch.nn.Module):
+    def __init__(self):
+        super(FCNModel, self).__init__()
+        self.layer1 = torch.nn.Linear(in_features=3, out_features=4)
+        self.layer2 = torch.nn.Linear()
+        self.layer3 = torch.nn.Linear()
+        self.output_layer = torch.nn.Linear()
+
+    def forward(self, x):
+        return x
+
+
+class SquishModel(torch.nn.Module):
+    def __init__(self):
+        super(SquishModel, self).__init__()
+        self.layer1 = torch.nn.Linear()
+        self.layer2 = torch.nn.Linear()
+        self.layer3 = torch.nn.Linear()
+        self.output_layer = torch.nn.Linear()
+
+    def forward(self, x):
+        print(x.shape)
+        return x
 
 
 if __name__ == "__main__":
@@ -82,33 +225,40 @@ if __name__ == "__main__":
         from combined net image parameters
         2. multi input for first block : https://rosenfelder.ai/multi-input-neural-network-pytorch/
     """
-    from torchvision import models
-
-    outputchannels = 2
+    # from torchvision import models
+    #
+    outputchannels = 255
     input_channels = 252
-    # image_shape = (252, 2000, 2000)
-    infer_input_model = INFERFeatureExtractor(252)
-    preset_model = models.segmentation.deeplabv3_resnet50(pretrained=False, num_classes=outputchannels, progress=True)
-    combined_model = CombinedModel(input_shape=input_channels, segmentation_model=preset_model,
-                                   n_classes=outputchannels)
-    print(summary(infer_input_model, (252, 96, 96)))
-    # print(summary(combined_model, (252, 96, 96)))
-    # exit()
-    import numpy as np
-
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(combined_model.parameters(), lr=0.001)
-    num_epochs = 1
-    combined_model.train()
-    for epoch in range(num_epochs):
-        input_data = torch.from_numpy(np.random.random((2, 252, 96, 96)).astype(np.float32))
-        target = torch.from_numpy((np.random.random((2, outputchannels, 96, 96)) > 0.3).astype(np.float32))
-        outputs = combined_model(input_data)
-        loss = criterion(outputs, target)
-        # Backward pass and optimization
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    image_shape = (252, 2000, 2000)
+    window = 200
+    # infer_input_model = LSTMModel(input_size=input_channels, hidden_size=32, num_layers=2,
+    #                               num_classes=outputchannels)
+    # infer_input_model = INFERFeatureExtractor1D(input_channels=input_channels, output_channels=outputchannels)
+    infer_input_model = CombinedModel(input_channels, None, outputchannels, window_size=200, batchsize=80)
+    print(summaryX(infer_input_model, torch.zeros((80, 252, window, window))))
+    # print(summary(infer_input_model, (252, window, window), batch_size=80))
+    # preset_model = models.segmentation.deeplabv3_resnet50(pretrained=False, num_classes=outputchannels,
+    #                                                       progress=True)
+    # combined_model = CombinedModel(input_shape=input_channels, segmentation_model=preset_model,
+    #                                n_classes=outputchannels, window_size=200)
+    # print(summary(infer_input_model, (252, 96, 96)))
+    # # print(summary(combined_model, (252, 96, 96)))
+    # # exit()
+    # import numpy as np
+    #
+    # criterion = torch.nn.CrossEntropyLoss()
+    # optimizer = torch.optim.Adam(combined_model.parameters(), lr=0.001)
+    # num_epochs = 1
+    # combined_model.train()
+    # for epoch in range(num_epochs):
+    #     input_data = torch.from_numpy(np.random.random((2, 252, 96, 96)).astype(np.float32))
+    #     target = torch.from_numpy((np.random.random((2, outputchannels, 96, 96)) > 0.3).astype(np.float32))
+    #     outputs = combined_model(input_data)
+    #     loss = criterion(outputs, target)
+    #     # Backward pass and optimization
+    #     optimizer.zero_grad()
+    #     loss.backward()
+    #     optimizer.step()
     # encoded = infer_input_model(input)
     # output = modelB(x2)
     # augmented_preset_model = CombinedModel().combine([infer_input_model, preset_model])
